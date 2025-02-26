ab310d10508933acb26a2751079e989caad38a6e
ashleybanks98 <ashley.banks@nihr.ac.uk>
2025-02-26 17:24:45 +0000

    Update chat_inf.py

--- chat_inf.py
+++ chat_inf.py
@@ -104,7 +104,7 @@ This tool allows you to query NIHR-supported projects and receive a summary of r
 
 **Please note::** 
 Language models can make errors, only use these results as an overview and validate prior to sharing further.
-Infrastructure supported projects only include those supported in 2022/23 and 2023/24/
+Infrastructure supported projects only include those supported in 2022/23 and 2023/24.
 
 """)
 
