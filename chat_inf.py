3937b7f5aa095b0e13e96017c222d2e9e20048e4
ashleybanks98 <ashley.banks@nihr.ac.uk>
2025-03-03 14:27:44 +0000

    Update chat_inf.py

--- chat_inf.py
+++ chat_inf.py
@@ -92,7 +92,7 @@ st.title("🔍 NIHR Project Query Tool")
 st.markdown("""
 This tool allows you to query NIHR-supported projects and receive a summary of research relevant to your query.  
 - Select **Infrastructure**, **Programmes**, or **Both**.  
-- Enter your **Google API Key** (can be generated at "https://aistudio.google.com/apikey").
+- Enter your reusable **Google API Key** (can be generated at "https://aistudio.google.com/apikey").
 - Enter your **query**, e.g., "novel drug delivery device".  
 - Specify the **focus** of the summary (optional), e.g., "the organisations undertaking the work", "do not talk about the researchers", or "group projects by their intended aim".  
 - Specify the **number of closest matches** to consider, use more for broader topics.  
